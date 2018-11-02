02ac0da590c1a9c78735036cc88ae17c1035f995
Maarten Blokker <maarten.blokker@debijenkorf.nl>
2018-11-02 13:15:28 +0100

    Added audio synthesis

--- backend/dialogflow/dialogflow-client.js
+++ backend/dialogflow/dialogflow-client.js
@@ -1,16 +1,19 @@
-// You can find your project ID in your Dialogflow agent settings
+const fs = require('fs');
+const path = require('path');
+const dialogflow = require('dialogflow').v2beta1;
+
+// Audio settings
+const FFplay = require("ffplay");
+const outputPath = './output.wav';
+
+// Dialogflow settings
 const projectId = 'acme-221110'; 
 const sessionId = 'acme-prototype-session';
-
 const query = 'give me shoes';
 const languageCode = 'en-US';
 
-// Instantiate a DialogFlow client.
-const fs = require('fs');
-const dialogflow = require('dialogflow').v2beta1;
-const sessionClient = new dialogflow.SessionsClient();
-
 // Define session path
+const sessionClient = new dialogflow.SessionsClient();
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
 console.log("setting up request")
@@ -55,14 +58,18 @@ sessionClient
 
 function playAudio(responses) {
     const audioFile = responses[0].outputAudio;
-    const outputFile = './test.wav';
+    const outputFile = path.resolve(outputPath);
+
+    console.log('writing file to: '+outputFile);
 
     fs.writeFile(outputFile, audioFile, 'binary', err => {
         if (err) {
           console.error('ERROR:', err);
           return;
         }
-        console.log(`Audio content written to file: ${outputFile}`);
+
+        var player = new FFplay(outputFile);
+        player.resume()
     });
 
     setTimeout(function(){console.log('bla')}, 1000);
