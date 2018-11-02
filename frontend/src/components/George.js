457a857bd30b545d08f08dc488fe3fa95cbdb71a
anjarupe <anja.rupnik@debijenkorf.nl>
2018-11-02 13:38:59 +0100

    created george

---
+++ frontend/src/components/George.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import George1 from '../../images/logoface'
+import George2 from '../../images/logoface_open'
+
+class George extends Component {
+  render() {
+    return (
+      <div>
+        <img src={ George1 } />
+      </div>
+    )
+  }
+}
+
+export default George
