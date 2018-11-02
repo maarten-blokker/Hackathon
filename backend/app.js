e2fe62c74d616572d1539b16ba8d304c512103cc
Maarten Blokker <maarten.blokker@debijenkorf.nl>
2018-11-02 12:35:05 +0100

    added socket io

--- backend/app.js
+++ backend/app.js
@@ -1,75 +1,15 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-
-
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const exphbs  = require('express-handlebars');
-
-const routes = require('./routes/index');
-const users = require('./routes/user');
-
-const app = express();
-
-const env = process.env.NODE_ENV || 'development';
-app.locals.ENV = env;
-app.locals.ENV_DEVELOPMENT = env == 'development';
-
-// view engine setup
-
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  partialsDir: ['views/partials/']
-}));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'handlebars');
-
-// app.use(favicon(__dirname + '/public/img/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use((err, req, res, next) => {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
-module.exports = app;
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
