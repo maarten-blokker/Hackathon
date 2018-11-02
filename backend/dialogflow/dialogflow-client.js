e6383247ed233afe7bdb7fbe6a030fff4310ae93
Maarten Blokker <maarten.blokker@debijenkorf.nl>
2018-11-02 13:35:59 +0100

    Added client logic

--- backend/dialogflow/dialogflow-client.js
+++ backend/dialogflow/dialogflow-client.js
@@ -7,70 +7,90 @@ const FFplay = require("ffplay");
 const outputPath = './output.wav';
 
 // Dialogflow settings
-const projectId = 'acme-221110'; 
+const projectId = 'acme-221110';
 const sessionId = 'acme-prototype-session';
-const query = 'give me shoes';
 const languageCode = 'en-US';
 
 // Define session path
 const sessionClient = new dialogflow.SessionsClient();
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-console.log("setting up request")
-
-// The audio query request.
-const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        text: query,
-        languageCode: languageCode,
-      },
-    },
-    outputAudioConfig: {
-      audioEncoding: `OUTPUT_AUDIO_ENCODING_LINEAR_16`,
-    },
-};
-
-// Send request and log result
-sessionClient
-  .detectIntent(request)
-  .then(responses => {
-    console.log('Detected intent');
-    const result = responses[0].queryResult;
-
-    playAudio(responses);
-
-    console.log(JSON.stringify(result))
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-    if (result.intent) {
-      console.log(`  Intent: ${result.intent.displayName}`);
-    } else {
-      console.log(`  No intent matched.`);
-    }
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
-
-
-
 function playAudio(responses) {
     const audioFile = responses[0].outputAudio;
     const outputFile = path.resolve(outputPath);
 
-    console.log('writing file to: '+outputFile);
+    console.log('writing file to: ' + outputFile);
 
     fs.writeFile(outputFile, audioFile, 'binary', err => {
         if (err) {
-          console.error('ERROR:', err);
-          return;
+            console.error('ERROR:', err);
+            return;
         }
 
+        console.log('Playing audio')
+        
         var player = new FFplay(outputFile);
         player.resume()
     });
+}
 
-    setTimeout(function(){console.log('bla')}, 1000);
+function createTextRequest(textQuery) {
+    return {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: textQuery,
+                languageCode: languageCode,
+            },
+        },
+        outputAudioConfig: {
+            audioEncoding: `OUTPUT_AUDIO_ENCODING_LINEAR_16`,
+        },
+    };
+}
+
+function executeTextQuery(textQuery, fn) {
+    sessionClient
+        .detectIntent(createTextRequest(textQuery))
+        .then(responses => {
+            console.log('Detected intent');
+            if (!result.intent) {
+                fn(null, 'no intent matched')
+            }
+
+            console.log(`Matched intent: ${result.intent.displayName}`);
+            
+            const response = handleResponse(responses);
+
+            fn(handleResponse(responses));
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+        });
+}
+
+function handleResponse(responses) {
+    const result = responses[0].queryResult;
+    const intent = result.intent.displayName;
+
+    playAudio(responses);
+
+    if (!intent) {
+        return null;
+    }
+
+    const params = {};
+    const fields = result.parameters.fields;
+    for (var key in fields) {
+        params[key] = fields[key].stringValue
+    }
+
+    return {
+        'intent': intent,
+        'params': params
+    };
+}
+
+module.exports = {
+    executeTextQuery
 }
